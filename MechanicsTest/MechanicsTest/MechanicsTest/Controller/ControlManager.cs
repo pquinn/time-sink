12286c698f42fe249577c06a8e0982c98d911aba
Stephen Elliott <steell1890@gmail.com>
2012-11-01 15:57:48 -0400

    PHYSICS
    Spacebar toggles gravity

---
+++ MechanicsTest/MechanicsTest/MechanicsTest/Controller/ControlManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+
+namespace MechanicsTest.Controller
+{
+    interface IKeyboardControllable
+    {
+        void HandleKeyboardInput(GameTime gameTime);
+    }
+}
