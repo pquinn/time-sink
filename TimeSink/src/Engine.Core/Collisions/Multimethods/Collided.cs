d0654e006f3082692b60f70e0dc1166f9fd457bb
Stephen Elliott <steell1890@gmail.com>
2012-11-05 12:04:07 -0500

    Revert "Revert "Fixed stutter on first collision""
    This reverts commit e277679224f6b1d3ecdafbc435683457d38d617f.

--- TimeSink/src/Engine.Core/Collisions/Multimethods/Collided.cs
+++ TimeSink/src/Engine.Core/Collisions/Multimethods/Collided.cs
@@ -7,7 +7,7 @@ using System.Reflection;
 namespace TimeSink.Engine.Core.Collisions
 {
 	// Dynamically dispatches to the best Collided overload.
-	internal static class Collided
+	public static class Collided
 	{
 		// Used to identify overloads. The overload method name may be anything
 		// but the signature should match Invoke except that both the return and
@@ -253,7 +253,7 @@ namespace TimeSink.Engine.Core.Collisions
 				return 0;
 		}
 		
-		private static void DoAutoRegister()
+		public static void DoAutoRegister()
 		{
 			ms_candidates = new List<Entry>();		// we want to set this even if no overloads are found
 			
