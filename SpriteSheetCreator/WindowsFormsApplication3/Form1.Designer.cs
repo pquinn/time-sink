b9bb07dda9036d46b8e30b93cf5963b1dddc6227
Nick Bagley <bagley.n@husky.neu.edu>
2012-11-10 12:39:51 -0500

    Added Sprite Sheet Creator

---
+++ SpriteSheetCreator/WindowsFormsApplication3/Form1.Designer.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace SpriteSheetCreator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Browse = new System.Windows.Forms.Button();
+            this.folderBrowser = new System.Windows.Forms.FolderBrowserDialog();
+            this.locBox = new System.Windows.Forms.TextBox();
+            this.OK = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // Browse
+            // 
+            this.Browse.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.Browse.Location = new System.Drawing.Point(11, 341);
+            this.Browse.Name = "Browse";
+            this.Browse.Size = new System.Drawing.Size(75, 23);
+            this.Browse.TabIndex = 0;
+            this.Browse.Text = "Browse";
+            this.Browse.UseVisualStyleBackColor = false;
+            this.Browse.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // locBox
+            // 
+            this.locBox.Location = new System.Drawing.Point(92, 343);
+            this.locBox.Name = "locBox";
+            this.locBox.Size = new System.Drawing.Size(235, 20);
+            this.locBox.TabIndex = 1;
+            // 
+            // OK
+            // 
+            this.OK.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.OK.Location = new System.Drawing.Point(576, 340);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(75, 23);
+            this.OK.TabIndex = 2;
+            this.OK.Text = "OK";
+            this.OK.UseVisualStyleBackColor = false;
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(456, 340);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            this.textBox1.Text = "FileName";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(663, 375);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.locBox);
+            this.Controls.Add(this.Browse);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private Button Browse;
+        private FolderBrowserDialog folderBrowser;
+        private TextBox locBox;
+        private PictureBox[] previews = new PictureBox[10];
+        private Button OK;
+        private TextBox textBox1;
+    }
+}
+
---
+++ SpriteSheetCreator/WindowsFormsApplication3/Form1.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SpriteSheetCreator
+{
+    public partial class Form1 : Form
+    {
+        string selectedFolder;
+        string destinationFolder;
+        int MAX_WIDTH;
+        int MAX_HEIGHT;
+        int TOTAL_IMAGES;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            DialogResult result = folderBrowser.ShowDialog();
+
+            locBox.Text = folderBrowser.SelectedPath;
+            if (result == DialogResult.OK)
+            {
+                selectedFolder = destinationFolder = folderBrowser.SelectedPath;
+                
+                AnalyzeFiles();
+            }
+        }
+
+        private void AnalyzeFiles()
+        {
+            string[] fileEntries = Directory.GetFiles(selectedFolder);
+            this.previews = new PictureBox[fileEntries.Count()];
+            this.TOTAL_IMAGES = fileEntries.Count();
+            int i = 0;
+
+            foreach (string file in fileEntries)
+            {
+                if (Path.GetExtension(file).Equals(".png"))
+                {
+                    Image img = Image.FromFile(file);
+                    if (img.Width > MAX_WIDTH)
+                        MAX_WIDTH = img.Width + 10;
+                    if (img.Height > MAX_HEIGHT)
+                        MAX_HEIGHT = img.Height;
+
+                    PictureBox pic = previews[i];
+
+                    if (pic == null)
+                    {
+                        pic = new PictureBox();
+                    }
+                    #region Init
+                    ((System.ComponentModel.ISupportInitialize)(pic)).BeginInit();
+                    pic.Width = 50;
+                    pic.Height = 100;
+                    pic.SizeMode = PictureBoxSizeMode.StretchImage;
+                    pic.Location = new Point(0 + (pic.Width * i), 50);
+                    pic.Visible = true;
+                    this.Controls.Add(pic);
+                    ((System.ComponentModel.ISupportInitialize)(pic)).EndInit();
+                    #endregion
+                    pic.Load(file);
+                    img.Dispose();
+                    i++;
+                }
+                else
+                    TOTAL_IMAGES--;
+            }
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+            if (selectedFolder != null)
+            {
+                Bitmap bmp = new Bitmap(MAX_WIDTH * TOTAL_IMAGES, MAX_HEIGHT);
+                string[] fileEntries = Directory.GetFiles(selectedFolder);
+                
+                int i = 0;
+                foreach (string file in fileEntries)
+                {
+                    if (Path.GetExtension(file).Equals(".png"))
+                    {
+                        Graphics g = Graphics.FromImage(bmp);
+                        Image img = Image.FromFile(file);
+                        g.DrawImage(img, ((MAX_WIDTH - img.Width) / 2) + (i * MAX_WIDTH), 0, img.Width, img.Height);
+                        g.Dispose();
+                        i++;
+                    }
+                }
+                bmp.Save(destinationFolder + "/" + textBox1.Text + ".PNG", System.Drawing.Imaging.ImageFormat.Png);
+                bmp.Dispose();
+                GenerateTextFile();
+            }
+        }
+
+        private void GenerateTextFile()
+        {
+            StreamWriter sw = new StreamWriter(destinationFolder + "/" + "SpriteInfo.txt");
+
+            sw.WriteLine("Maximum Frame Width : " + MAX_WIDTH);
+            sw.WriteLine("Maximum Frame Height : " + MAX_HEIGHT);
+            sw.WriteLine("Number of Frames : " + TOTAL_IMAGES);
+
+            sw.Dispose();
+        }
+    }
+}
