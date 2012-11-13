d185b3893a54de246ee75305792414db8b64c306
Phil Quinn <phillmatic19@gmail.com>
2012-11-12 20:42:33 -0500

    Inventory stuff, F to fire, G to switch items

--- TimeSink/src/Engine.Game/Engine.Game/Entities/Weapons/Arrow.cs
+++ TimeSink/src/Engine.Game/Engine.Game/Entities/Weapons/Arrow.cs
@@ -4,15 +4,24 @@ using TimeSink.Engine.Core;
 using TimeSink.Engine.Core.Collisions;
 using TimeSink.Engine.Core.Physics;
 using TimeSink.Engine.Core.Rendering;
+using TimeSink.Engine.Core.Input;
+using Microsoft.Xna.Framework.Input;
+using System;
 
 namespace TimeSink.Engine.Game.Entities.Weapons
 {
-    public class Arrow : Entity
+    public class Arrow : Entity, IWeapon
     {
         const float ARROW_MASS = 1f;
         const string ARROW_TEXTURE_NAME = "Textures/Weapons/Arrow";
 
+        const float MAX_ARROW_HOLD = 1;
+        const float MIN_ARROW_INIT_SPEED = 500;
+        const float MAX_ARROW_INIT_SPEED = 1500;
+
         public GravityPhysics physics { get; private set; }
+
+        public Arrow() { }
 
         public Arrow(Vector2 position)
         {
@@ -87,6 +96,27 @@ namespace TimeSink.Engine.Game.Entities.Weapons
                 world.CollisionManager.UnregisterCollisionBody(this);
                 world.PhysicsManager.UnregisterPhysicsBody(this);
             }
+        }
+
+        public void Fire(UserControlledCharacter character, EngineGame world, GameTime gameTime, double holdTime)
+        {
+            character.InHold = false;
+            Arrow arrow = new Arrow(
+                new Vector2(character.PhysicsController.Position.X + UserControlledCharacter.X_OFFSET,
+                            character.PhysicsController.Position.Y + UserControlledCharacter.Y_OFFSET));
+            var elapsedTime = Math.Min(gameTime.TotalGameTime.TotalSeconds - holdTime, MAX_ARROW_HOLD);
+            // linear interp: y = 500 + (x - 0)(1300 - 500)/(MAX_HOLD-0) x = elapsedTime
+            float speed =
+                MIN_ARROW_INIT_SPEED + (MAX_ARROW_INIT_SPEED - MIN_ARROW_INIT_SPEED) /
+                                       MAX_ARROW_HOLD *
+                                       (float)elapsedTime;
+            Vector2 initialVelocity = speed * character.Direction;
+            arrow.physics.Velocity += initialVelocity;
+            world.Entities.Add(arrow);
+            world.RenderManager.RegisterRenderable(arrow);
+            world.PhysicsManager.RegisterPhysicsBody(arrow);
+            world.CollisionManager.RegisterCollisionBody(arrow);
+
         }
     }
 }
