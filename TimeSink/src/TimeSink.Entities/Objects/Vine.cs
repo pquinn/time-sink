83a68b76256f081259c6e93b5a7c7832b8e4fce2
Phil Quinn <phillmatic19@gmail.com>
2012-11-27 16:27:45 -0500

    vine changes

--- TimeSink/src/TimeSink.Entities/Objects/Vine.cs
+++ TimeSink/src/TimeSink.Entities/Objects/Vine.cs
@@ -29,6 +29,8 @@ namespace TimeSink.Entities.Objects
         protected int textureWidth;
         protected float scale;
 
+        public float Rotation { get; set; }
+
         public Vector2 Size { get; set; }
 
         protected Vector2 _initialPosition;
@@ -38,7 +40,8 @@ namespace TimeSink.Entities.Objects
         public Vine(Vector2 position)
         {
             _initialPosition = position;
-            scale = .25f;
+            scale = .5f;
+            Rotation = 0f;
         }
 
         [SerializableField]
@@ -102,7 +105,7 @@ namespace TimeSink.Entities.Objects
                 return new BasicRendering(
                     VINE_TEXTURE,
                     PhysicsConstants.MetersToPixels(Physics.Position),
-                    0f,
+                    Rotation,
                     new Vector2(scale, scale));
             }
         }
@@ -110,6 +113,20 @@ namespace TimeSink.Entities.Objects
         public override List<Fixture> CollisionGeometry
         {
             get { return Physics.FixtureList; }
+        }
+
+        public override void Update(GameTime time, EngineGame world)
+        {
+            // SLERP(P0, P1, t): (1 - t)p0 + t*p1
+            // interpolation might be unnecessary here since were updating the rotation directly
+
+
+            // just interpolate rotation like a line!
+            float piOver64 = MathHelper.PiOver4 / 16;
+            if (Rotation < MathHelper.PiOver4 && Rotation > -MathHelper.PiOver4)
+            {
+                Rotation += piOver64;
+            }
         }
     }
 }
