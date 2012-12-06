fb1ea0be42d07120be5330db8c5cdef2690a2909
Phil Quinn <phillmatic19@gmail.com>
2012-12-05 20:47:26 -0500

    centipedes kind of work but not entirely

--- TimeSink/src/TimeSink.Entities/Enemies/NormalCentipede.cs
+++ TimeSink/src/TimeSink.Entities/Enemies/NormalCentipede.cs
@@ -71,7 +71,7 @@ namespace TimeSink.Entities.Enemies
                 return new TintedRendering(
                   CENTIPEDE_TEXTURE,
                   PhysicsConstants.MetersToPixels(Physics.Position),
-                  0,
+                  Physics.Rotation,
                   Vector2.One,
                   new Color(255f, tint, tint, 255f));
             }
@@ -88,41 +88,134 @@ namespace TimeSink.Entities.Enemies
         [OnCollidedWith.Overload]
         public bool OnCollidedWith(WorldGeometry2 wg, Contact info)
         {
-            Physics.IgnoreGravity = true; 
+            Physics.IgnoreGravity = true;
             return true;
         }
 
+        private bool collidedAhead = false;
+        private bool collidedDownward = false;
+        private bool needToTurn = false;
+        private bool needToTransformCW = false;
+        private bool needToTransformCCW = false;
+        private Vector2 forwardTop = Vector2.Zero;
+        private Vector2 forwardBottom = Vector2.Zero;
+        private Vector2 xDirectionCast = Vector2.Zero;
+        private Vector2 yDirectionCast = Vector2.Zero;
+        private bool initialized = false;
         public override void OnUpdate(GameTime time, EngineGame world)
         {
             base.OnUpdate(time, world);
 
+            //if (Physics.IgnoreGravity == false)
+            //{
+            //    return;
+            //}
+
+            var widthMeters = PhysicsConstants.PixelsToMeters(Width);
+            var heightMeters = PhysicsConstants.PixelsToMeters(Height);
+
             Physics.Position += PatrolDirection * (float)time.ElapsedGameTime.TotalSeconds;
 
-            var forwardBottom = Physics.Position + new Vector2(
-                PatrolDirection.X >= 0 ? PhysicsConstants.PixelsToMeters(textureWidth) / 2 : -PhysicsConstants.PixelsToMeters(textureWidth) / 2, 
-                PhysicsConstants.PixelsToMeters(textureHeight) / 2);
+            var xDirection = (int)PatrolDirection.X >= 0 ? 1 : -1;
+            var yDirection = (int)PatrolDirection.Y >= 0 ? -1 : 1;
 
+            var totalRotationMatrix = Matrix.CreateRotationZ(Physics.Rotation);
+            var quarterTurnCWMatrix = Matrix.CreateRotationZ(MathHelper.PiOver2);
+            var quarterTurnCCWMatrix = Matrix.CreateRotationZ(-MathHelper.PiOver2);
 
-            var forwardTop = Physics.Position + new Vector2(
-                PatrolDirection.X >= 0 ? PhysicsConstants.PixelsToMeters(textureWidth) / 2 : -PhysicsConstants.PixelsToMeters(textureWidth) / 2,
-                -PhysicsConstants.PixelsToMeters(textureHeight) / 2);
+            if (!initialized)
+            {
+                forwardTop = Physics.Position + new Vector2(
+                    xDirection * PhysicsConstants.PixelsToMeters(Width) / 2,
+                    -PhysicsConstants.PixelsToMeters(Height) / 2);
+                forwardBottom = Physics.Position + new Vector2(
+                    xDirection * PhysicsConstants.PixelsToMeters(Width) / 2,
+                    PhysicsConstants.PixelsToMeters(Height) / 2);
 
-            var collidedDownward = false;
-            var collidedAhead = false;
+            xDirectionCast = new Vector2(xDirection * .1f, 0);
+            yDirectionCast = new Vector2(0, yDirection * .1f);
 
-            //cast a ray downward from bottom front corner
+                initialized = true;
+            }
+
+            if (needToTurn)
+            {
+                var topOffset = new Vector2(
+                    xDirection * PhysicsConstants.PixelsToMeters(Width) / 4,
+                    -PhysicsConstants.PixelsToMeters(Height) / 2);
+                forwardTop = Physics.Position + topOffset;
+
+                var botOffset = new Vector2(
+                    xDirection * PhysicsConstants.PixelsToMeters(Width) / 4,
+                    PhysicsConstants.PixelsToMeters(Height) / 2);
+                forwardBottom = Physics.Position + botOffset;
+            }
+            else
+            {
+                var topOffset = new Vector2(
+                    xDirection * PhysicsConstants.PixelsToMeters(Width) / 2,
+                    -PhysicsConstants.PixelsToMeters(Height) / 2);
+                forwardTop = Physics.Position + topOffset;
+
+                var botOffset = new Vector2(
+                    xDirection * PhysicsConstants.PixelsToMeters(Width) / 2,
+                    PhysicsConstants.PixelsToMeters(Height) / 2);
+                forwardBottom = Physics.Position + botOffset;
+            }
+
+            if (needToTransformCW)
+            {
+                Physics.Rotation += MathHelper.PiOver2;
+                PatrolDirection = Vector2.Transform(PatrolDirection, quarterTurnCWMatrix);
+                forwardTop = Vector2.Transform(forwardTop, quarterTurnCWMatrix);
+                forwardBottom = Vector2.Transform(forwardBottom, quarterTurnCWMatrix);
+                xDirectionCast = Vector2.Transform(xDirectionCast, quarterTurnCWMatrix);
+                yDirectionCast = Vector2.Transform(yDirectionCast, quarterTurnCWMatrix);
+                //forwardTop.Y *= -1;
+                //forwardBottom.Y *= -1;
+                needToTransformCW = false;
+            }
+
+            if (needToTransformCCW)
+            {
+                Physics.Rotation -= MathHelper.PiOver2;
+                PatrolDirection = Vector2.Transform(PatrolDirection, quarterTurnCCWMatrix);
+                forwardTop = Vector2.Transform(forwardTop, quarterTurnCCWMatrix);
+                forwardBottom = Vector2.Transform(forwardBottom, quarterTurnCCWMatrix);
+                xDirectionCast = Vector2.Transform(xDirectionCast, quarterTurnCCWMatrix);
+                yDirectionCast = Vector2.Transform(yDirectionCast, quarterTurnCCWMatrix);
+                //forwardTop.Y *= -1;
+                //forwardBottom.Y *= -1;
+                needToTransformCCW = false;
+            }
+
+            ////cast a ray forward from top front corner
+            //world.LevelManager.PhysicsManager.World.RayCast(
+            //    delegate(Fixture fixture, Vector2 point, Vector2 normal, float fraction)
+            //    {
+            //        // do what it do here
+            //        //Console.WriteLine("Front Top Forward Ray Cast Callback");
+            //        collidedAhead = true;
+            //        return 0;
+            //    },
+            //    Vector2.Transform(forwardTop, totalRotationMatrix),
+            //    Vector2.Transform(forwardTop + xDirectionCast, totalRotationMatrix));
+
             world.LevelManager.PhysicsManager.World.RayCast(
                 delegate(Fixture fixture, Vector2 point, Vector2 normal, float fraction)
                 {
                     // do what it do here
-                    Console.WriteLine("Front Top Forward Ray Cast Callback");
+                    //Console.WriteLine("Front Top Forward Ray Cast Callback");
                     collidedAhead = true;
                     return 0;
                 },
-                forwardBottom,
-                forwardBottom + new Vector2(0, .1f));
+                forwardTop,
+                forwardTop + xDirectionCast);
 
-            //cast a ray downward from bottom front corner
+            //var temp1 = Vector2.Transform(forwardTop + xDirectionCast, totalRotationMatrix);
+            //var temp2 = Vector2.Transform(xDirectionCast, totalRotationMatrix);
+
+            //cast a ray downward from bottom front corner 
             world.LevelManager.PhysicsManager.World.RayCast(
                 delegate(Fixture fixture, Vector2 point, Vector2 normal, float fraction)
                 {
@@ -132,23 +225,54 @@ namespace TimeSink.Entities.Enemies
                     return 0;
                 },
                 forwardBottom,
-                forwardBottom + new Vector2(0, .1f));
+                forwardBottom + yDirectionCast);
 
-            var anyCollision = collidedDownward || collidedAhead;
-
-            // handle collided logic here
-            if (anyCollision)
+            if (collidedAhead)
             {
-                PatrolDirection *= -Vector2.UnitX;
+                if (needToTurn)
+                {
+                    //Physics.Rotation += MathHelper.PiOver2;
+                    //PatrolDirection = Vector2.Transform(PatrolDirection, quarterTurnCWMatrix);
+                    needToTransformCW = true;
+                    needToTurn = false;
+
+                    //Physics.Rotation += MathHelper.PiOver2;
+                    //PatrolDirection = Vector2.Transform(PatrolDirection, quarterTurnCWMatrix);
+                    //forwardTop = Vector2.Transform(forwardTop, quarterTurnCWMatrix);
+                    //forwardBottom = Vector2.Transform(forwardBottom, quarterTurnCWMatrix);
+                    //xDirectionCast = Vector2.Transform(xDirectionCast, quarterTurnCWMatrix);
+                    //yDirectionCast = Vector2.Transform(yDirectionCast, quarterTurnCWMatrix);
+                    //needToTurn = false;
+                }
+                else
+                {
+                    needToTurn = true;
+                }
+
+                collidedAhead = false;
             }
+
+            //if (!collidedDownward && Physics.IgnoreGravity == true)
+            //{
+            //    if (needToTurn)
+            //    {
+            //        Physics.Rotation -= MathHelper.PiOver2;
+            //        PatrolDirection = Vector2.Transform(PatrolDirection, quarterTurnCCWMatrix);
+            //        needToTransformCCW = true;
+            //        needToTurn = false;
+            //    }
+            //    else
+            //    {
+            //        needToTurn = true;
+            //    }
+            //    collidedDownward = true;
+            //}
         }
 
         public override void Load(IComponentContext engineRegistrations)
         {
             var textureCache = engineRegistrations.Resolve<IResourceCache<Texture2D>>();
             var texture = textureCache.LoadResource(CENTIPEDE_TEXTURE);
-            textureWidth = texture.Width;
-            textureHeight = texture.Height;
         }
 
         protected override Texture2D GetTexture(IResourceCache<Texture2D> textureCache)
@@ -159,8 +283,7 @@ namespace TimeSink.Entities.Enemies
         public override void InitializePhysics(bool force, IComponentContext engineRegistrations)
         {
             base.InitializePhysics(force, engineRegistrations);
-
             //Physics.IgnoreGravity = true;
-        } 
+        }
     }
 }
