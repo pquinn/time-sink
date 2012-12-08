4fa5f26fb36855af719880625d598fcfc14cbaec
Kyle <billemeyer.k@gmail.com>
2012-12-08 15:24:31 -0500

    added door template

---
+++ TimeSink/src/TimeSink.Entities/Objects/Door.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TimeSink.Engine.Core;
+
+namespace TimeSink.Entities.Objects
+{
+    public class Door : Entity
+    {
+    }
+}
