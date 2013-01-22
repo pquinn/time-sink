ec82f3501c397abd9353d07b07c5d531292d9950
Nick Bagley <bagley.n@husky.neu.edu>
2013-01-22 16:38:06 -0500

    Tutorial Display milestone

---
+++ TimeSink/src/Engine.Core/Rendering/TextRendering.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TimeSink.Engine.Core.Caching;
+
+namespace TimeSink.Engine.Core.Rendering
+{
+    public class TextRendering : IRendering
+    {
+        protected Vector2 position;
+        protected float rotation;
+        protected Vector2 scale;
+        protected String text;
+
+        public TextRendering(String text, Vector2 position, float rotation, Vector2 scale)
+        {
+            this.text = text;
+            this.position = position;
+            this.rotation = rotation;
+            this.scale = scale;
+        }
+        public virtual void Draw(SpriteBatch spriteBatch, IResourceCache<Texture2D> cache, Matrix globalTransform)
+        {
+
+            spriteBatch.Begin(
+                SpriteSortMode.BackToFront,
+                BlendState.AlphaBlend,
+                null,
+                null,
+                null,
+                null,
+                globalTransform);
+
+
+
+
+            spriteBatch.DrawString(EngineGame.Instance.ScreenManager.Font, text, position, Color.Green);
+            spriteBatch.End();
+        }
+
+        public bool Contains(Vector2 point, IResourceCache<Texture2D> cache, Matrix transform)
+        {
+            return false;
+        }
+
+        public Vector2 GetCenter(IResourceCache<Texture2D> cache, Matrix transform)
+        {
+            return Vector2.One;
+        }
+
+        public NonAxisAlignedBoundingBox GetNonAxisAlignedBoundingBox(IResourceCache<Texture2D> cache, Matrix globalTransform)
+        {
+            return new NonAxisAlignedBoundingBox();
+        }
+    }
+}
---
+++ TimeSink/src/Engine.Core/TutorialDisplay.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TimeSink.Engine.Core.Rendering;
+
+namespace TimeSink.Engine.Core
+{
+   public class TutorialDisplay : IRenderable
+    {
+       private Vector2 position;
+       private String text;
+
+       public TutorialDisplay(String text, Vector2 position)
+       {
+           this.text = text;
+           this.position = position;
+       }
+
+       public void FadeText()
+       {
+       }
+
+        public IRendering Rendering
+        {
+            get { return new TextRendering(text, new Vector2(position.X, position.Y - 100), 0, Vector2.One); }
+        }
+    }
+}
