d7793d85222870563ca08f75689452e861433c38
pquinn <phillmatic19@gmail.com>
2013-02-04 22:26:29 -0500

    dialogue utilities in python

--- resources/dialogue/random_data_generator.py
+++ resources/dialogue/random_data_generator.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 from random import randint
+from data_structures import *
 import uuid, sys
 
 # make object to hold dialogue
@@ -83,6 +84,7 @@ class DataGenerator:
             next_entry = self.prompt_objects[randint(0, len(self.prompt_objects)-1)].the_id
             response_object = ResponseObject(
                 the_id,
+                "",
                 entry,
                 next_entry
             )
@@ -118,57 +120,6 @@ class DataGenerator:
 
     def get_random_element(self, input_list):
         return input_list[randint(0, len(input_list)-1)]
-
-class PromptObject():
-    def __init__(self, the_id, speaker, entry, animation, sound, quest, response_required):
-        self.the_id = the_id
-        self.speaker = speaker
-        self.entry = entry
-        self.animation = animation
-        self.sound = sound
-        self.quest = quest
-        self.response_required = response_required
-        
-    def to_insert(self):
-        # "The sum of 1 + 2 is {0}".format(1+2)
-        sql = "INSERT INTO Prompt VALUES(\"{0}\", \"{1}\", {2}, {3}, {4}, {5}, {6});".format(
-            self.the_id,
-            self.speaker,
-            "null" if self.entry == "null" else "\"{0}\"".format(self.entry),
-            "null" if self.animation == "null" else "\"{0}\"".format(self.animation),
-            "null" if self.sound == "null" else "\"{0}\"".format(self.sound),
-            "null" if self.quest == "null" else "\"{0}\"".format(self.quest),
-            self.response_required
-            )
-        return sql
-
-class ResponseObject():
-    def __init__(self, the_id, entry, next_entry):
-        self.the_id = the_id
-        self.entry = entry
-        self.next_entry = next_entry
-        
-    def to_insert(self):
-        sql = "INSERT INTO Response VALUES(\"{0}\", \"{1}\", \"{2}\");".format(
-            self.the_id,
-            self.entry,
-            self.next_entry
-            )
-        return sql
-        
-class ResponseMapObject():
-    def __init__(self, the_id, from_id, to_id):
-        self.the_id = the_id
-        self.from_id = from_id
-        self.to_id = to_id
-        
-    def to_insert(self):
-        sql = "INSERT INTO Response_Map VALUES(\"{0}\", \"{1}\", \"{2}\");".format(
-            self.the_id,
-            self.from_id,
-            self.to_id
-            )
-        return sql
 
 def main(argv):
     data_generator = DataGenerator()
