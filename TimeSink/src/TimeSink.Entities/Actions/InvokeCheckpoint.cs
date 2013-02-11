cf5ccdd26eadb218cce95a2f74945d945b88da1a
Nick Bagley <bagley.n@husky.neu.edu>
2013-02-10 23:32:33 -0500

    Checkpoint anim

--- TimeSink/src/TimeSink.Entities/Actions/InvokeCheckpoint.cs
+++ TimeSink/src/TimeSink.Entities/Actions/InvokeCheckpoint.cs
@@ -7,6 +7,8 @@ using TimeSink.Engine.Core.Editor;
 using TimeSink.Engine.Core.States;
 using Microsoft.Xna.Framework;
 using TimeSink.Engine.Core;
+using TimeSink.Engine.Core.Rendering;
+using TimeSink.Engine.Core.Physics;
 
 namespace TimeSink.Entities.Actions
 {
@@ -16,6 +18,12 @@ namespace TimeSink.Entities.Actions
     {
         const string EDITOR_NAME = "Checkpoint";
         private static readonly Guid guid = new Guid("5487de5f-acba-42c9-9404-b05ddea64b02");
+        const string TEXTURE = "Textures/Objects/Checkpoint";
+        private NewAnimationRendering rendering;
+        private static readonly Vector2 SrcRectSize = new Vector2(200f, 304f);
+        private float timer;
+        private float interval = 150f;
+        private bool activated = false;
 
         public InvokeCheckpoint()
             : this(Vector2.Zero, 50, 50)
@@ -25,12 +33,41 @@ namespace TimeSink.Entities.Actions
         public InvokeCheckpoint(Vector2 position, int width, int height)
             : base(position, width, height)
         {
+            rendering = new NewAnimationRendering(
+              TEXTURE,
+              SrcRectSize,
+              10,
+              PhysicsConstants.MetersToPixels(position),
+              0,
+              new Vector2(.5f, .5f),
+              Color.White) { DepthWithinLayer = 100 };
         }
 
         public override Guid Id
         {
             get { return guid; }
             set { }
+        }
+
+
+        public override IRendering Preview
+        {
+            get
+            {
+                return new BasicRendering(TEXTURE)
+                {
+                    Position = PhysicsConstants.MetersToPixels(Position),
+                    Scale = BasicRendering.CreateScaleFromSize(Width, Height, TEXTURE, TextureCache),
+                    DepthWithinLayer = -100
+                };
+            }
+        }
+        public override IRendering Rendering
+        {
+            get
+            {
+                return rendering;
+            }
         }
 
         public override string EditorName
@@ -47,6 +84,20 @@ namespace TimeSink.Entities.Actions
             engine.LevelManager.LevelCache.ReplaceOrAdd(
                 "Save",
                 new Save(Engine.LevelManager.LevelPath, SpawnPoint, Character.Health, Character.Mana, Character.Inventory));
+            activated = true;
+        }
+
+        public override void OnUpdate(GameTime time, EngineGame world)
+        {
+            timer += (float)time.ElapsedGameTime.TotalMilliseconds;
+
+            if (timer >= interval && activated && rendering.CurrentFrame != rendering.NumFrames - 1)
+            {
+                rendering.CurrentFrame = (rendering.CurrentFrame + 1) % rendering.NumFrames;
+                timer = 0f;
+            }
+            rendering.Position = PhysicsConstants.MetersToPixels(position);
+
         }
     }
 }
